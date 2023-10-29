6f030f03dd02edf1e149d64be6e072bf5aca8e79
AwesomeDev777 <133025209+AwesomeDev777@users.noreply.github.com>
2023-10-30 07:20:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('', createProxyMiddleware({ target: 'https://development.videoo.dev', changeOrigin: true }));
+app.use('', createProxyMiddleware({ target: 'https://bcgame.im', changeOrigin: true }));
 app.listen(3000);
